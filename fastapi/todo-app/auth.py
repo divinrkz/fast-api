f1f889b1a86f6263caedc86f47c3bca1d5786dc7
divinirakiza <irakizadivin@gmail.com>
2022-04-21 13:52:28 +0200

    [feat]: added fastapi base models

---
+++ fastapi/todo-app/auth.py
@@ -0,0 +1,107 @@
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from pydantic import BaseModel
+from typing import Optional
+import models
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from db import SessionLocal, engine
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+
+SECRET_KEY = "743IUEJDK8934WEIUFHKSDJNUUI"
+ALGO = 'HS256'
+
+
+class CreateUser(BaseModel):
+    username: str
+    email: Optional[str]
+    first_name: str
+    last_name: str
+    password: str
+
+
+bcrypt_context = CryptContext(schemas=['bcrypt'], depreciated="auto")
+
+models.Base.metadata.create_all(bind=engine)
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='token')
+
+app = FastAPI()
+
+
+def get_db():
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+def get_password_hash(password):
+    return bcrypt_context.hash(password)
+
+
+def verify_password(plain_password, hashed_passowrd):
+    return bcrypt_context.verify(plain_password, hashed_passowrd)
+
+
+def authenticate_user(username: str, password: str, db: Session = Depends(get_db)):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+
+def create_access_token(username: str, user_id: int, expires_delta: Optional[timedelta] = None):
+    encode = {'sub': username, 'id': user_id}
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    encode.udpate({'exp': expire})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGO)
+
+
+@app.post('/create/user')
+async def create_new_user(create_user: CreateUser, db: Session = Depends(get_db)):
+    create_user_model = models.Users()
+    create_user_model.email = create_user.email
+    create_user_model.username = create_user.username
+    create_user_model.first_name = create_user.first_name
+    create_user_model.last_name = create_user.last_name
+    create_user_model.hashed_password = get_password_hash(create_user.password)
+    create_user_model.is_active = True
+
+    db.add(create_user_model)
+    db.commit()
+
+
+async def get_current_user(token: str = Depends(oauth2_bearer)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGO])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+
+        if username is None or user_id is None:
+            raise HTTPException(status_code=404, detail='User not found')
+        return {'username': username, 'id': user_id}
+    except JWTError:
+        raise HTTPException(status_code=404, detail='User not found')
+
+
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    token_expires = timedelta(minutes=20)
+    token = create_access_token(user.username, user.id, expires_delta=token_expires)
+    return {'token': token}
+
+    return {
+        'status': 201,
+        'message': 'Successful'
+    }
--- fastapi/todo-app/models.py
+++ fastapi/todo-app/models.py
@@ -1,11 +1,27 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from db import Base
+from sqlalchemy.orm import relationship
 
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(Integer, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    todos = relationship("Todos", back_populates="owner")
 
 class Todos(Base):
     __tablename__ = 'todos'
-    id = Column(Integer, primary_ke=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey("users.id"))
+
+    owner = relationship("Users", back_populates="todos")
